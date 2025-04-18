73d35a5e2eeef50f8c0c5b593b2216c06f2ce74b
Orsofja <orlov.gennadij@gmail.com>
2025-04-18 19:02:00 +0300

    update files

--- logic.py
+++ logic.py
@@ -12,7 +12,7 @@ class DB_Manager:
     def create_tables(self):
         conn = sqlite3.connect(self.database)
         with conn:
-            conn.execute("""CREATE TABLE projects(
+            conn.execute("""CREATE TABLE results(
                         result_id INTEGER PRIMARY KEY,
                         user_id INTEGER,
                         user_name TEXT NOT NULL,
@@ -63,3 +63,78 @@ class DB_Manager:
         sql = 'INSERT OR IGNORE INTO quiz3 (quiz3_name) values(?)'
         data = quiz3_answers
         self.__executemany(sql, data)
+
+    def insert_result(self, data):
+        sql = """INSERT INTO results
+        (user_id, user_name, question1, question2, question3, quiz1, quiz2, quiz3) 
+        values(?, ?, ?, ?)""" 
+        self.__executemany(sql, data)
+
+    def get_quiz1(self):
+        sql="SELECT quiz1_name from quiz1" 
+        return self.__select_data(sql)
+        
+    def get_quiz2(self):
+        sql="SELECT quiz2_name from quiz2" 
+        return self.__select_data(sql)
+    
+    def get_quiz3(self):
+        sql="SELECT quiz3_name from quiz3" 
+        return self.__select_data(sql)
+
+    def get_quiz1_id(self, quiz1_name):
+        sql = 'SELECT quiz1_id FROM status WHERE quiz1_name = ?'
+        res = self.__select_data(sql, (quiz1_name,))
+        if res: return res[0][0]
+        else: return None
+
+    def get_quiz2_id(self, quiz2_name):
+        sql = 'SELECT quiz2_id FROM status WHERE quiz2_name = ?'
+        res = self.__select_data(sql, (quiz2_name,))
+        if res: return res[0][0]
+        else: return None
+
+
+    def get_quiz3_id(self, quiz3_name):
+        sql = 'SELECT quiz3_id FROM status WHERE quiz3_name = ?'
+        res = self.__select_data(sql, (quiz3_name,))
+        if res: return res[0][0]
+        else: return None
+
+    def get_results(self, user_id):
+        sql="""SELECT * FROM results 
+        WHERE user_id = ?""" 
+        return self.__select_data(sql, data = (user_id,))
+        
+    def get_result_id(self, user_name, user_id):
+        return self.__select_data(sql='SELECT reslut_id FROM results WHERE user_name = ? AND user_id = ?  ', data = (user_name, user_id,))[0][0]
+      
+    def get_result_info(self, user_id, user_name):
+        sql = """
+        SELECT user_name, questiion1, question2, question3, quiz1_name, quiz2_name, quiz3_name FROM projects 
+        JOIN quiz1 ON
+        quiz1.quiz1_id = results.quiz1_id
+        JOIN quiz2 ON
+        quiz2.quiz2_id = results.quiz2_id
+        JOIN quiz3 ON
+        quiz3.quiz3_id = results.quiz3_id
+        WHERE user_name=? AND user_id=?
+        """
+        return self.__select_data(sql=sql, data = (user_name, user_id))
+
+
+    def update_results(self, param, data):
+        sql = f"""UPDATE results SET {param} = ? 
+        WHERE user_name = ? AND user_id = ?""" 
+        self.__executemany(sql, [data]) 
+
+
+    def delete_result(self, user_id, project_id):
+        sql = """DELETE FROM results 
+        WHERE user_id = ? AND result_id = ? """ 
+        self.__executemany(sql, [(user_id, project_id)])
+
+if __name__ == '__main__':
+    manager = DB_Manager(DATABASE)
+    manager.create_tables()
+    manager.default_insert()
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from logic import DB_Manager
+from logic import *
 from config import *
 from telebot import TeleBot
 from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, KeyboardButton
@@ -7,9 +7,38 @@ from telebot import types
 bot = TeleBot(TOKEN)
 hideBoard = types.ReplyKeyboardRemove() 
 
-def no_results(message):
-    bot.send_message(message.chat.id, 'У тебя пока нет проектов!\nМожешь добавить их с помошью команды /new_project')
 
+cancel_button = "Отмена 🚫"
+def no_results(message):
+    bot.send_message(message.chat.id, 'Ты ещё не проходил тест!\nМожешь пройти его с помошью команды /complete_test')
+
+def gen_inline_markup(rows):
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 1
+    for row in rows:
+        markup.add(InlineKeyboardButton(row, callback_data=row))
+    return markup
+
+def gen_markup(rows):
+    markup = ReplyKeyboardMarkup(one_time_keyboard=True)
+    markup.row_width = 1
+    for row in rows:
+        markup.add(KeyboardButton(row))
+    markup.add(KeyboardButton(cancel_button))
+    return markup
+
+attributes_of_projects = {'Имя пользователя' : ["Как к тебе обращаться?", "project_name"],
+                          "Ответ на 1 вопрос" : ["Какие у тебя хобби?", "description"],
+                          "Ответ на 2 вопрос" : ["", "url"],
+                          "Ответ на 3 вопрос" : ["", "status_id"]} #доработка
+
+def info_project(message, user_id, project_name): #доработка
+    info = manager.get_project_info(user_id, project_name)[0]
+    bot.send_message(message.chat.id, f"""Project name: {info[0]}
+Description: {info[1]}
+Link: {info[2]}
+Status: {info[3]}
+""")
 
 @bot.message_handler(commands=['start'])
 def start_command(message):
@@ -38,5 +67,72 @@ def name_user(message):
     name = message.text
     user_id = message.from_user.id
     data = [user_id, name]
+    bot.send_message(message.chat.id, "Отлично! Теперь расскажи немного о себе! Какие у тебя хобби?")
+    bot.register_next_step_handler(message, save_answer1, data=data)
 
+def save_answer1(message, data):
+    data.append(message.text)
+    bot.send_message(message.chat.id, "Интересно! А скажешь, ?")
+    bot.register_next_step_handler(message, save_answer2, data=data)
+
+def save_answer2(message, data):
+    data.append(message.text)
+    bot.send_message(message.chat.id, "Ого! А как ты думаешь, ?")
+    bot.register_next_step_handler(message, save_answer3, data=data)
+
+def save_answer3(message, data): 
+    data.append(message.text)
+    quiz1 = [x[0] for x in manager.get_quiz1()] 
+    bot.send_message(message.chat.id, "А теперь выбери свою возрастную группу:", reply_markup=gen_markup(quiz1))
+    bot.register_next_step_handler(message, callback_quiz1, data=data, quiz1=quiz1)
+
+def callback_quiz1(message, data, quiz1_answers): 
+    quiz1 = message.text
+    if message.text == cancel_button:
+        no_results(message)
+        return
+    if quiz1 not in quiz1_answers:
+        bot.send_message(message.chat.id, "Ты выбрал ответ не из списка, попробуй еще раз!)", reply_markup=gen_markup(quiz1))
+        bot.register_next_step_handler(message, callback_quiz1, data=data, quiz1=quiz1)
+        return
+    quiz1_id = manager.get_quiz1_id(quiz1_answers)
+    data.append(quiz1_id)
+    quiz2 = [x[0] for x in manager.get_quiz2()] 
+    bot.send_message(message.chat.id, "Теперь выбери свою образовательную группу:", )
+    bot.register_next_step_handler(message, callback_quiz2, data=data, quiz2=quiz2)
+
+def callback_quiz2(message, data, quiz2_answers): 
+    quiz2 = message.text
+    if message.text == cancel_button:
+        no_results(message)
+        return
+    if quiz2 not in quiz2_answers:
+        bot.send_message(message.chat.id, "Ты выбрал ответ не из списка, попробуй еще раз!)", reply_markup=gen_markup(quiz2))
+        bot.register_next_step_handler(message, callback_quiz2, data=data, quiz2=quiz2)
+        return
+    quiz2_id = manager.get_quiz2_id(quiz2_answers)
+    data.append(quiz2_id)
+    quiz3 = [x[0] for x in manager.get_quiz3()] 
+    bot.send_message(message.chat.id, "И на последок :", )
+    bot.register_next_step_handler(message, callback_quiz3, data=data, quiz3=quiz3)
+
+def callback_quiz3(message, data, quiz2_answers): 
+    quiz3 = message.text
+    if message.text == cancel_button:
+        no_results(message)
+        return
+    if quiz3 not in quiz3_answers:
+        bot.send_message(message.chat.id, "Ты выбрал ответ не из списка, попробуй еще раз!)", reply_markup=gen_markup(quiz3))
+        bot.register_next_step_handler(message, callback_quiz3, data=data, quiz3=quiz3)
+        return
+    quiz3_id = manager.get_quiz3_id(quiz3_answers)
+    data.append(quiz3_id)
+    manager.insert_result([tuple(data)])
+    bot.send_message(message.chat.id, "Ваши результаты сохранены! Можете посмотреть их по комманде /results!")
+
+#дальнейшее в разработке
+
+if __name__ == '__main__':
+    manager = DB_Manager(DATABASE)
+    bot.infinity_polling()
 
